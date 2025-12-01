7e22c49bf74189885dc7b2e1a0c4be53c71f6a14
joshmoritty <josh.moritty@gmail.com>
2025-12-01 10:31:06 +0700

    refactor: delete unused code

--- src/main/java/com/jitech/demo/repository/GuestRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.jitech.demo.repository;
-//
-//import com.jitech.demo.model.GuestUsers;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface GuestRepository extends JpaRepository<GuestUsers, Integer> {
-//
-//}
