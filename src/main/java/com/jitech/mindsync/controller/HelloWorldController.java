1d59435aaa9afe13456d46ed1092ca8942ba0ed5
joshmoritty <josh.moritty@gmail.com>
2025-12-01 13:25:20 +0700

    chore: add temporary helloworld controller for testing

---
+++ src/main/java/com/jitech/mindsync/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.jitech.mindsync.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public Map<String, String> helloWorld() {
+        return Map.of("message", "Hello World");
+    }
+}
