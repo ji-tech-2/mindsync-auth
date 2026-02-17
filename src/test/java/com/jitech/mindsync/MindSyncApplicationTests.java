db083c352158038057242f5e72117108694c9ffc
joshmoritty <josh.moritty@gmail.com>
2026-02-17 07:53:07 +0700

    test: update test properties

--- src/test/java/com/jitech/mindsync/MindSyncApplicationTests.java
+++ src/test/java/com/jitech/mindsync/MindSyncApplicationTests.java
@@ -3,8 +3,10 @@ package com.jitech.mindsync;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(initializers = TestJwtPropertiesInitializer.class)
 @ActiveProfiles("test")
 class MindSyncApplicationTests {
 
