b7ee6abf24b1403edc17d82e8cf735b1840ab105
claudiahann <claudia.mamahit@binus.ac.id>
2026-02-24 15:47:46 +0700

    feat: add seed in table gender, occupation, and work remote

--- src/main/resources/db/migration/V1__create_genders.sql
+++ src/main/resources/db/migration/V1__create_genders.sql
@@ -3,3 +3,6 @@ CREATE TABLE genders (
     gender_id INT PRIMARY KEY AUTO_INCREMENT,
     gender_name VARCHAR(50) NOT NULL UNIQUE
 );
+
+-- Seed data
+INSERT INTO genders (gender_name) VALUES ('Female'), ('Male'), ('Non-binary/Other');
--- src/main/resources/db/migration/V2__create_occupations.sql
+++ src/main/resources/db/migration/V2__create_occupations.sql
@@ -3,3 +3,6 @@ CREATE TABLE occupations (
     occupation_id INT PRIMARY KEY AUTO_INCREMENT,
     occupation_name VARCHAR(100) NOT NULL UNIQUE
 );
+
+-- Seed data
+INSERT INTO occupations (occupation_name) VALUES ('Employed'), ('Student'), ('Self-employed'), ('Retired'), ('Unemployed');
